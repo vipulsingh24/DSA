8ce476a9a1cc875c91b411b34d420028146e4dbb
vipulsingh24 <letsmailvipul@gmail.com>
2021-08-28 23:30:59 +0530

    Maximum Length of a Concatenated String with Unique Characters

--- Problems/max_unique_chars.py
+++ Problems/max_unique_chars.py
@@ -1,3 +1,38 @@
+"""
+Maximum Length of a Concatenated String with Unique Characters
+"""
+
+def max_length(arr):
+    result = [-1]
+
+    max_unique_chars_length(arr, "", 0, result)
+    if result[0] != -1:
+        return result[0]
+
+    return 0
+
+
+def max_unique_chars_length(arr, curr_str, index, result):
+    print("\nCurrent str: ", curr_str)
+    print("Index: ", index)
+    print("Result: ", result)
+    # base case
+    if index == len(arr):
+        return
+
+    for index in range(index, len(arr)):
+        print("Loop index: ", index)
+        concat_str = curr_str + arr[index]
+        print("Concat str: ", concat_str)
+        if len(set(concat_str)) == len(list(concat_str)):
+            print("Len match")
+            if len(concat_str) > result[0]:
+                result[0] = len(concat_str)
+
+            max_unique_chars_length(arr, concat_str, index + 1, result)
+
+
+"""
 def is_unique_char(s):
     set_ = set()
     for i in s:
@@ -32,7 +67,7 @@ def max_length(arr):
         length = length if length > len(str_) else  len(str_)
 
     return length
-
+"""
 
 if __name__ == "__main__":
     s = ["", "ab", "cd", "ab"]

8a4f579dd5eacee74984a4323613c2a87c51e881
Vipul Singh <letsmailvipul@gmail.com>
2023-11-27 18:20:58 +0530

    Dynamic Programming fibonacci problem

---
+++ DynamicProgramming/fibonacci.py
@@ -0,0 +1,19 @@
+memo = {}
+def fib(n):
+    if n in memo:
+        return memo[n]
+    
+    if n <= 2:
+        result = 1
+    else:
+        result = fib(n - 1) + fib(n - 2)
+        
+    memo[n] = result
+    return result
+
+
+print(fib(7))
+print(fib(50))
+
+
+## Running complexity: O(N)

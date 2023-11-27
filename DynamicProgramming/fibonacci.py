01b8d774eb0e7d5116958751e4e5e352544061d4
Vipul Singh <letsmailvipul@gmail.com>
2023-11-27 18:23:32 +0530

    Dynamic Programming memoization approach fibonacci problem

--- DynamicProgramming/fibonacci.py
+++ DynamicProgramming/fibonacci.py
@@ -17,3 +17,17 @@ print(fib(50))
 
 
 ## Running complexity: O(N)
+
+
+## Bottom-Up Approach
+
+def fib(n):
+    memo  = {}
+    for i in range(1, n + 1):
+        if i <= 2:
+            result = 1
+        else:
+            result = memo[i - 1] + memo[i - 2]
+        memo[i] = result
+
+    return memo[n]
